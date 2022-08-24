a32f712ecb8cfcb26a9a352191fd25077dfc3033
Tyler Batkin <111218991+dippio@users.noreply.github.com>
2022-08-24 18:41:18 +1000

    change a few things
    mainly just that it now installs python 3.6 instead of latest (for compatibility with a few packages)
    also installs some packages via pip n stuff now

--- README.md
+++ README.md
@@ -4,12 +4,22 @@
 
 i prefer linux over windows, i just gotta reinstall it for school sometimes
 
-using a package manager is just more comfortable and quicker for me
+this makes it slightly less nightmareish to get everything set up
 
 ## what it does
-installs a bunch of programs and tools via chocolatey (also installs the package manager in question)
+does a bunch of stuff
+<details>
+  <summary>(what it actually does)</summary>
+    - installs chocolatey
+    - updates windows (and drivers / firmware if supported)
+    - installs some cli utilities (curl, git, sudo, winfetch)
+    - installs python 3.6.7 and pandas, pillow, numpy, matplotlib and pygame
+    - installs some general programs (spotify, obs, libreoffice, audacity, etc)
+</details>
 
 ## how to use
-run 'install.ps1' as admin
+run:
+'Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force; ls -Recurse *.ps*1 | Unblock-File; .\install.ps1' 
+in an admin powershell environment
 
 accept any admin requests that pop up
--- install.ps1
+++ install.ps1
@@ -25,12 +25,18 @@ function Install-Choco
 #these are the programs i generally use the most on a new pc, i normally can just go from this point onward from memory, add / remove anything to ur hearts content though
 function Install-ChocoPrograms 
 {
-    choco install sudo, winfetch, brave-nightly --pre, paint.net, python3, vscode, audacity, obs, git, curl, libreoffice, qbittorrent, spotify, spicetify-cli, vlc, winscp, discord, betterdiscord, steam, vcredist2015, vcredist2017, directx -y
+    choco install sudo, winfetch, brave-nightly --pre, paint.net, python --version=3.6.7, vscode, audacity, obs, git, curl, libreoffice, qbittorrent, spotify, spicetify-cli, vlc, winscp, discord, betterdiscord, steam, vcredist2015, vcredist2017, directx -y
 }
 
 function Update-Packages
 {
     choco upgrade all -y
+}
+
+#installs some stuff i use for python
+function Install-PipPrograms
+{
+    pip install numpy matplotlib pygame pandas Pillow
 }
 
 Write-Host "Attemtping to install Chocolatey..."
@@ -42,4 +48,6 @@ Write-Host "Attempting to download main programs via choco."
 Install-ChocoPrograms
 Write-Host "All packages downloaded. Attemtping to update just incase."
 Update-Packages
+Write-Host "Attempting to install some stuff via pip"
+Install-PipPrograms
 Write-Host "Everything has been completed."
